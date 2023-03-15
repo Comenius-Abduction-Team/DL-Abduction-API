2527fe6ca3cce05c1b0c6a4da6d6d71a7af066f5
jakloo <kloc.jkb@gmail.com>
2023-03-15 18:55:35 +0100

    slight naming changes

--- src/main/java/abductionapi/factories/AbducibleContainerFactory.java
+++ src/main/java/abductionapi/factories/AbducibleContainerFactory.java
@@ -6,7 +6,6 @@ import abductionapi.abducibles.AbducibleContainer;
  * AbductionManagerAndAbducibleContainerFactory generic interface.
  * @author Zuzana Hlávková, hlavkovazuz@gmail.com
  *
- * @param <ABDUCTION_MANAGER> AbductionManager type parameter
  * @param <ABDUCIBLE_CONTAINER> AbducibleContainer type parameter
  */
 public interface AbducibleContainerFactory<ABDUCIBLE_CONTAINER extends AbducibleContainer> {
@@ -15,5 +14,5 @@ public interface AbducibleContainerFactory<ABDUCIBLE_CONTAINER extends Abducible
      * Creates an abduction manager.
      * @return a new instance of AbductionManager.
      */
-    public ABDUCIBLE_CONTAINER createAuducibleCOntainer();
+    ABDUCIBLE_CONTAINER createAuducibleContainer();
 }
--- src/main/java/abductionapi/manager/MultiObservationManager.java
+++ src/main/java/abductionapi/manager/MultiObservationManager.java
@@ -1,6 +1,6 @@
 package abductionapi.manager;
 
-import abductionapi.exception.AxiomObservationException;
+import abductionapi.exception.InvalidObservationException;
 import abductionapi.exception.MultiObservationException;
 import org.semanticweb.owlapi.model.OWLAxiom;
 
@@ -18,14 +18,14 @@ public interface MultiObservationManager {
      * Sets multi observation for abduction.
      * @param observation multi observation for abduction.
      * @throws MultiObservationException if solver does not support multi observation.
-     * @throws AxiomObservationException if solver does not support this type of observation axiom.
+     * @throws InvalidObservationException if solver does not support this type of observation axiom.
      */
-    default void setObservations(Set<OWLAxiom> observation) throws AxiomObservationException { }
+    default void setObservations(Set<OWLAxiom> observation) throws InvalidObservationException { }
 
     /**
      * Returns observation.
      * @return <OBSERVATION_TYPE> observation.
      */
-    public Set<OWLAxiom> getObservations();
+    Set<OWLAxiom> getObservations();
 
 }
