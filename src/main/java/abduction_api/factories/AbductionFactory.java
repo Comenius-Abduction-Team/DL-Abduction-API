d5d796936a00da18a7ad853f238dc9f840d97dc1
jakloo <kloc.jkb@gmail.com>
2023-05-07 11:06:18 +0200

    removed generics from factory

--- src/main/java/abduction_api/factories/AbductionFactory.java
+++ src/main/java/abduction_api/factories/AbductionFactory.java
@@ -2,7 +2,9 @@ package abduction_api.factories;
 
 import abduction_api.abducibles.AxiomAbducibleContainer;
 import abduction_api.abducibles.SymbolAbducibleContainer;
+import abduction_api.exception.AxiomAbducibleException;
 import abduction_api.exception.NotSupportedException;
+import abduction_api.exception.SymbolAbducibleException;
 import abduction_api.manager.AbductionManager;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLEntity;
@@ -10,39 +12,29 @@ import org.semanticweb.owlapi.model.OWLOntology;
 
 import java.util.Set;
 
-public interface AbductionFactory<
-        ABDUCTION_MANAGER extends AbductionManager,
-        SYMBOL_ABDUCIBLES extends SymbolAbducibleContainer,
-        AXIOM_ABDUCIBLES extends AxiomAbducibleContainer
-        > {
-
-    ABDUCTION_MANAGER getSolverSpecificAbductionManager();
+public interface AbductionFactory {
 
     AbductionManager getAbductionManager();
 
     AbductionManager getAbductionManagerWithInput(OWLOntology knowledgeBase, OWLAxiom observation);
 
-    default AbductionManager getAbductionManagerWithSymbolAbducibles(Set<OWLEntity> symbols){
+    default AbductionManager getAbductionManagerWithSymbolAbducibles(Set<OWLEntity> symbols)
+            throws NotSupportedException, SymbolAbducibleException {
         throw new NotSupportedException("symbol abducibles");
     }
 
-    default AbductionManager getAbductionManagerWithAxiomAbducibles(Set<OWLAxiom> axioms){
+    default AbductionManager getAbductionManagerWithAxiomAbducibles(Set<OWLAxiom> axioms)
+            throws NotSupportedException, AxiomAbducibleException {
         throw new NotSupportedException("axiom abducibles");
     }
 
-    default AxiomAbducibleContainer getAxiomAbducibleContainer(){
-        throw new NotSupportedException("axiom abducibles");
-    }
-
-    default SymbolAbducibleContainer getSymbolAbducibleContainer(){
+    default SymbolAbducibleContainer getSymbolAbducibleContainer()
+            throws NotSupportedException, SymbolAbducibleException {
         throw new NotSupportedException("symbol abducibles");
     }
 
-    default AXIOM_ABDUCIBLES getSolverSpecificAxiomAbducibleContainer(){
+    default AxiomAbducibleContainer getAxiomAbducibleContainer()
+            throws NotSupportedException, AxiomAbducibleException {
         throw new NotSupportedException("axiom abducibles");
-    }
-
-    default SYMBOL_ABDUCIBLES getSolverSpecificSymbolAbducibleContainer(){
-        throw new NotSupportedException("symbol abducibles");
     }
 }
