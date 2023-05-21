0007538acb64e34d091d6a007d019f44731f3b74
jakloo <kloc.jkb@gmail.com>
2023-05-21 14:52:36 +0200

    added monitor wait limit and getter/setter in abducible containers

--- src/main/java/abduction_api/monitor/AbductionMonitor.java
+++ src/main/java/abduction_api/monitor/AbductionMonitor.java
@@ -33,7 +33,7 @@ public class AbductionMonitor {
         newProgressAvailable = true;
     }
 
-    public boolean areNewExplanationsAvailable() {
+    public boolean isNewExplanationsAvailable() {
         return newExplanationsAvailable;
     }
 
